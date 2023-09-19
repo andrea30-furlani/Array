f78c5e2e11e977cd26dec6940c8a52101b29634c
ANDREA  CRISTINA REINO FURLANI <0220832221007@fatectq.local>
2023-09-19 12:13:42 -0300

    pilha

---
+++ pilha.js
@@ -0,0 +1,58 @@
+class pilha {
+ constructor()   {
+    this.items = [];
+ }
+
+ push(element) {
+    this.items.push(element); // push = adiciona na pilha
+ }
+
+pop() {
+    return this.items.pop(); //pop = retira da pilha
+    }
+
+peek() {
+    return this.items[this.items.length - 1]; // mostra a posição do último elemento
+    }
+
+qualeoprimeiro(){
+    return this.items[0];
+}
+
+totalElementos(){
+    return this.items.length;
+}
+
+size(){
+    return this.items.length;
+}
+
+isEmpty() {
+    return this.items.length === 0;
+}
+
+}
+
+const p = new pilha(); 
+
+p.push(10)
+p.push(5)
+p.push(15)
+console.log(p);
+console.log('valor do ultimo elemento: ' , p.peek());
+
+p.pop()
+console.log(p);
+console.log('valor do ultimo elemento: ' , p.peek());
+console.log('valor do ultimo elemento: ' , p.isEmpty());
+
+console.log('valor do primeiro elemento: ' , p.qualeoprimeiro());
+
+
+
+
+
+
+
+
+console.log(pilha)
